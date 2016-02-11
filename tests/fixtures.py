758b3998a75b78097d942f807de81fb3bce7a7b2
jathanism <jathan@gmail.com>
2016-02-11 02:30:26 +0000

    Enhanced support for viewing/updating objects by "natural key".
    Fixes #66
    
    Detailed changes:
    
    App
    
    - pynsot.commands.callbacks.list_endpoint() renamed to list_subcommand()
    - pynsot.commands.callbacks.get_resource_by_natural_key() factored out
      for eas of use by list_subcommand()
    - A bunch of variables renamed for readability
    - pysnot.app.list() expanded to try lookup by id first, then
      natural_key, then params.
    - pysnot.app.get_single_object() enhanced to support natural_key lookups
      for all resources.
    
    Attributes
    
    - lookup by name, resource_name will display verbosely
    - 'id' added to verbose display
    
    Devices
    
    - Lookup by hostname will display verbosely
    - You may now update a device by providing -H/--hostname
    - -i/--id is no longer required, unless you wish to update the hostname
      for the device.
    
    Networks
    
    - lookup by cidr will display verbosely
    - -c/--cidr added to update for lookup only
    - You may now update a device by providing -c/--cidr
    - -i/--id is no longer required unless -c/--cidr is not provided
    
    Interfaces
    
    - lookup by name, device will display verbosely
    - 'id' added to verbose display
    - No changes yet made or tested for updating interfaces
    
    Unit Tests
    - All hard-coded output tests replaced with content matching. It's just
      too tedious to test the output so explicitly.

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -74,6 +74,18 @@ DEVICE_UPDATE = {u'data': {u'device': {u'attributes': {u'monitored': u'',
    u'site_id': 1}},
  u'status': u'ok'}
 
+DEVICE_HOSTNAME_RETRIEVE = {u'data': {u'devices': [{u'attributes': {
+   u'owner': u'jathan'},
+   u'hostname': u'foo-bar1',
+   u'id': 1,
+   u'site_id': 1}],
+   'total': 1,
+   'offset': 0,
+   'limit': None,
+   },
+ u'status': u'ok'}
+
+
 # Networks
 NETWORK_CREATE = {'data': {'network': {'attributes': {'owner': 'jathan'},
    'id': 1,
@@ -132,6 +144,23 @@ NETWORKS_RESPONSE = {u'data': {u'limit': None,
   u'total': 2},
  u'status': u'ok'}
 
+NETWORK_CIDR_RETRIEVE = {u'data':
+  {u'networks': [{u'attributes': {u'owner': u'jathan'},
+   u'id': 1,
+   u'ip_version': u'4',
+   u'is_ip': False,
+   u'network_address': u'10.0.0.0',
+   u'parent_id': None,
+   u'prefix_length': 8,
+   u'state': 'allocated',
+   u'site_id': 1}],
+   "total": 1,
+   "limit": None,
+   "offset": 0,
+   },
+ u'status': u'ok'}
+
+
 # When retrieving Attributes.
 ATTRIBUTE_CREATE = {u'data': {u'attribute': {u'constraints': {u'allow_empty': False,
     u'pattern': u'',
@@ -183,6 +212,44 @@ ATTRIBUTES_RESPONSE = {u'data': {u'attributes': [{u'constraints': {u'allow_empty
   u'offset': 0,
   u'total': 3},
  u'status': u'ok'}
+
+ATTRIBUTES_NAME_RESPONSE = {u'data': {u'attributes': [{u'constraints': {u'allow_empty': True,
+     u'pattern': u'',
+     u'valid_values': []},
+    u'description': u'',
+    u'display': False,
+    u'id': 2,
+    u'multi': False,
+    u'name': u'monitored',
+    u'required': False,
+    u'resource_name': u'Device',
+    u'site_id': 1},],
+  u'limit': None,
+  u'offset': 0,
+  u'total': 1},
+ u'status': u'ok'}
+
+ATTRIBUTES_ID_RESPONSE = {
+    "status": "ok",
+    "data": {
+        "attribute": {
+            "multi": False,
+            "resource_name": "Device",
+            "description": "",
+            "display": False,
+            "required": False,
+            "site_id": 1,
+            "id": 2,
+            "constraints": {
+                "pattern": "",
+                "valid_values": [],
+                "allow_empty": True
+            },
+            "name": "monitored"
+        }
+    }
+}
+
 
 # Dummy config data used for testing dotfile and client
 CONFIG_DATA = {
