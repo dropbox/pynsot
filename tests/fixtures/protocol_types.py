860d349f54d296d0f8cd0497a55c1139bb188a84
Krishelle Hardson-Hurley <khardson@khardson-mbp.corp.dropbox.com>
2018-03-15 17:55:10 -0700

    Oops! Forgot to add this fixture.

---
+++ tests/fixtures/protocol_types.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, unicode_literals
+
+import pytest
+
+from tests.fixtures import site_client
+
+@pytest.fixture
+def protocol_type(site_client, protocol_attribute, protocol_attribute2):
+    """
+    Return a Protocol Type Object.
+    """
+    return site_client.sites(site_client.default_site).protocol_types.post(
+        {
+            'name': 'bgp',
+            'required_attributes': ['boo', 'foo',],
+            'description': 'bgp is my bestie',
+        }
+    )
+
+
+@pytest.fixture
+def protocol_attribute(site_client):
+    return site_client.sites(site_client.default_site).attributes.post(
+        {
+            'name':'boo',
+            'value': 'test_attribute',
+            'resource_name': 'Protocol',
+        }
+    )
+
+@pytest.fixture
+def protocol_attribute2(site_client):
+    return site_client.sites(site_client.default_site).attributes.post(
+        {
+            'name':'foo',
+            'value': 'test_attribute',
+            'resource_name': 'Protocol',
+        }
+    )
