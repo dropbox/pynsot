eef8522831e6b53739f75117a5f11624decf4a95
Ryan Heffernan <ryanh@dropbox.com>
2019-10-23 11:08:39 -0700

    fix indentation

--- conftest.py
+++ conftest.py
@@ -4,4 +4,4 @@ import sys
 import os
 
 if sys.version_info[0] < 3:
-   os.environ["DJANGO_SETTINGS_MODULE"] = "tests.nsot_settings"
+    os.environ["DJANGO_SETTINGS_MODULE"] = "tests.nsot_settings"
